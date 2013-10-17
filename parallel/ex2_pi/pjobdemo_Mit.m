8e5123e8eaa37ba3d8274e883b979cd2431e9ab8
mitabb <mabbasi1@sheffield.ac.uk>
2013-10-17 05:13:42 -0700

    pjobdemo updates

--- parallel/ex2_pi/pjobdemo_Mit.m
+++ parallel/ex2_pi/pjobdemo_Mit.m
@@ -1,24 +1,35 @@
 %simple demo script for matlab dce
 %test by Mike Griffiths 22nd January 2007
-%updated by Mitra 10th September 2013 
+%updated by Mitra 17th October 2013 
 
 %In old examples function findreasource has been used instead of parcluster
 %function.
-resource = findResource('scheduler', 'configuration', 'sge');
+%resource = findResource('scheduler', 'configuration', 'sge');
+
 
 %In matlab2013a parcluster is used to create cluster object
 %resource = parcluster('scheduler', 'configuration', 'sge');
-set(resource, 'configuration', 'sge');
+%set(resource, 'configuration', 'sge');
+
+defaultProfile=parallel.defaultClusterProfile
+resource=parcluster(defaultProfile);
 
 %defaultProfile=parallel.defaultClusterProfile
 %resource=parcluster(defaultProfile);
 %In matlab2013a createCommunicatingJob has been replaced with
 %createParallelJob.
-parjob=createParallelJob(resource);
+%parjob=createParallelJob(resource);
 
-%parjob=createCommunicatingJob(resource);
-set(parjob,'MinimumNumberOfWorkers',4);
-set(parjob,'MaximumNumberOfWorkers',4);
+parjob=createCommunicatingJob(resource);
+
+% In the new release, use NumWorkersRange instead of NumWorkersRange and MaximumNumberOfWorkers
+%set(parjob,'MinimumNumberOfWorkers',4);
+%set(parjob,'MaximumNumberOfWorkers',4);
+
+%Set the number of workers required for parallel execution:
+parjob.NumWorkersRange = [4 4];
+
+
 %createTask(parjob, 'rand', 1, {3});
 %createTask(parjob, 'myrandpar', 2, {5});
 createTask(parjob, 'quadpi', 1, {});
@@ -28,9 +39,13 @@ submit(parjob);
 %wait for job to complete before continuing
 %new release waitForState changed to wait
 %waitForState(job);
-wait(job);
+wait(parjob);
 
-parout = getAllOutputArguments(parjob);
+% In the new release fetchOutputs is replaced with getAllOutputArguments
+%parout = getAllOutputArguments(parjob);
+
+parout = parjob.fetchOutputs;
+
 
 save('results1.mat', 'parout', '-v6');
 %a1=parout{1,1};
