0a5735d3d3ea306a10e6b4166f2f1f56df379d95
mitabb <mabbasi1@sheffield.ac.uk>
2014-12-05 12:32:44 +0000

    mitro pjobdemo update

--- parallel/ex2_pi/pjobdemo.m
+++ parallel/ex2_pi/pjobdemo.m
@@ -1,24 +1,51 @@
 %simple demo script for matlab dce
 %test by Mike Griffiths 22nd January 2007
-resource = findResource('scheduler', 'configuration', 'sge');
-set(resource, 'configuration', 'sge');
-%resource=findResource('scheduler','type','generic');
-%set(resource, 'SubmitFcn', @sgeSubmitFcn);
-%set(resource, 'ParallelSubmitFcn', @sgeParallelBatchSubmitFcn);
-%set(resource, 'ParallelSubmitFcn', @sgeParallelSubmitFcn);
-%set(resource, 'ClusterMatlabRoot', '/usr/local/packages/matlab_mdcdem_r06b');
+%updated by Mitra 5th December 2014  
 
-parjob=createParallelJob(resource);
-set(parjob,'MinimumNumberOfWorkers',4);
-set(parjob,'MaximumNumberOfWorkers',4);
+%In old examples function findreasource has been used instead of parcluster
+%function.
+%resource = findResource('scheduler', 'configuration', 'sge');
+
+
+%In matlab2013a parcluster is used to create cluster object
+%resource = parcluster('scheduler', 'configuration', 'sge');
+%set(resource, 'configuration', 'sge');
+
+defaultProfile=parallel.defaultClusterProfile;
+resource=parcluster(defaultProfile);
+
+%defaultProfile=parallel.defaultClusterProfile
+%resource=parcluster(defaultProfile);
+%In matlab2013a createCommunicatingJob has been replaced with
+%createParallelJob.
+%parjob=createParallelJob(resource);
+
+parjob=createCommunicatingJob(resource);
+
+% In the new release, use NumWorkersRange instead of NumWorkersRange and MaximumNumberOfWorkers
+%set(parjob,'MinimumNumberOfWorkers',4);
+%set(parjob,'MaximumNumberOfWorkers',4);
+
+%Set the number of workers required for parallel execution:
+parjob.NumWorkersRange = [4 4];
+
+
 %createTask(parjob, 'rand', 1, {3});
 %createTask(parjob, 'myrandpar', 2, {5});
 createTask(parjob, 'quadpi', 1, {});
 
 submit(parjob);
 
-waitForState(parjob);
-parout = getAllOutputArguments(parjob);
+%wait for job to complete before continuing
+%new release waitForState changed to wait
+%waitForState(job);
+wait(parjob);
+
+% In the new release fetchOutputs is replaced with getAllOutputArguments
+%parout = getAllOutputArguments(parjob);
+
+parout = parjob.fetchOutputs;
+
 
 save('results1.mat', 'parout', '-v6');
 %a1=parout{1,1};
