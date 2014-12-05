e36a4ea3d6460202b5f89be70078ce06e6f3099e
mitabb <mabbasi1@sheffield.ac.uk>
2014-12-05 12:41:44 +0000

    mitra qscattersigma_dct1 update

--- parallel/ex3_parmatlab_qscatter/qscattersigma_dct1.m
+++ parallel/ex3_parmatlab_qscatter/qscattersigma_dct1.m
@@ -2,34 +2,71 @@
 %partial wave analysis of scattering
 %simple demo script for matlab dce
 %test by Mike Griffiths 22nd January 2007
-
+%updated by Mitra 5th December 2014 
 
 deltah=0.01;
 nsteps=200;
-parconfig='local';
+%parconfig='sge';
 numprocs=4;
 
-resource = findResource('scheduler', 'configuration', parconfig);
-set(resource, 'configuration', parconfig);
+
+%In old examples function findreasource has been used instead of parcluster
+%function.
+%resource = findResource('scheduler', 'configuration', parconfig);
+
+%In matlab2013a parcluster is used to create cluster object
+%resource = parcluster('scheduler', 'configuration', 'sge');
+%set(resource, 'configuration', 'sge');
+
+defaultProfile=parallel.defaultClusterProfile;
+resource=parcluster(defaultProfile);
 
 %set(resource, 'SubmitFcn', @sgeSubmitFcn);
 %set(resource, 'ParallelSubmitFcn', @sgeParallelSubmitFcn);
-%set(resource, 'ClusterMatlabRoot', '/usr/local/packages5/matlab_r2007b');
-parjob=createParallelJob(resource);
+%set(resource, 'ClusterMatlabRoot', '/usr/local/packages6/matlab/R2013a');
 
-set(parjob,'MinimumNumberOfWorkers',numprocs);
-set(parjob,'MaximumNumberOfWorkers',numprocs);
+%In matlab2013a createCommunicatingJob has been replaced with
+%createParallelJob.
+%parjob=createParallelJob(resource);
+
+parjob=createCommunicatingJob(resource);
+
+
+% In the new release, use NumWorkersRange instead of NumWorkersRange and MaximumNumberOfWorkers
+%set(parjob,'MinimumNumberOfWorkers',numprocs);
+%set(parjob,'MaximumNumberOfWorkers',numprocs);
+
+%Set the number of workers required for parallel execution:
+parjob.NumWorkersRange = [4 4];
+
+
+
 createTask(parjob, 'qscatterpar1', 2, {1});
 
 
 submit(parjob);
 tic
-waitForState(parjob);
-myruntime=toc
-parout = getAllOutputArguments(parjob);
+
+%wait for job to complete before continuing
+%new release waitForState changed to wait
+%waitForState(job);
+wait(parjob);
+
+
+myruntime=toc;
+
+% In the new release fetchOutputs is replaced with getAllOutputArguments
+%parout = getAllOutputArguments(parjob);
+
+parout = parjob.fetchOutputs;
+
 
 %save('results1.mat', 'parout', '-v6');
 sumouter=parout{1,1};
-destroy(parjob);
+
+% In new release delete is replaced with destroy
+%destroy(parjob);
+delete(parjob);
 plot(sumouter);
+
 
